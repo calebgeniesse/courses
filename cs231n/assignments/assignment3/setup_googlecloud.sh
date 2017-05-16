92acda933adbdaa93f0aef9e1103b0ce9ef107bd
calebgeniesse <calebgeniesse@gmail.com>
2017-05-16 06:35:31 +0000

    add -y flag conda install command lines in setup_googlecloud.sh

--- cs231n/assignments/assignment3/setup_googlecloud.sh
+++ cs231n/assignments/assignment3/setup_googlecloud.sh
@@ -7,10 +7,10 @@ sudo apt-get install python-dev
 sudo apt-get install python-pip
 sudo apt-get install libjpeg8-dev
 sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
-conda install pillow
+conda install -y pillow
 sudo apt-get build-dep python-imaging
 sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
-conda install --file requirements.txt  # Install dependencies
+conda install -y --file requirements.txt  # Install dependencies
 echo "**************************************************"
 echo "*****  End of Google Cloud Set-up Script  ********"
 echo "**************************************************"
