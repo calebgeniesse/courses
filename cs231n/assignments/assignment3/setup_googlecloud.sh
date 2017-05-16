4a7a326b8c8b9d71af6fda974e52930320d5717b
calebgeniesse <calebgeniesse@gmail.com>
2017-05-16 06:39:11 +0000

    run all install commands with --yes flag in setup_googlecloud.sh

--- cs231n/assignments/assignment3/setup_googlecloud.sh
+++ cs231n/assignments/assignment3/setup_googlecloud.sh
@@ -2,15 +2,15 @@
 
 # This is the set-up script for Google Cloud.
 sudo apt-get update
-sudo apt-get install libncurses5-dev
-sudo apt-get install python-dev
-sudo apt-get install python-pip
-sudo apt-get install libjpeg8-dev
+sudo apt-get install --yes libncurses5-dev
+sudo apt-get install --yes python-dev
+sudo apt-get install --yes python-pip
+sudo apt-get install --yes libjpeg8-dev
 sudo ln -s /usr/lib/x86_64-linux-gnu/libjpeg.so /usr/lib
-conda install -y pillow
+conda install --yes pillow
 sudo apt-get build-dep python-imaging
-sudo apt-get install libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
-conda install -y --file requirements.txt  # Install dependencies
+sudo apt-get install --yes libjpeg8 libjpeg62-dev libfreetype6 libfreetype6-dev
+conda install --yes --file requirements.txt  # Install dependencies
 echo "**************************************************"
 echo "*****  End of Google Cloud Set-up Script  ********"
 echo "**************************************************"
